65f63850170929d41c3e43c0321ff2a788e948c1
marcio_maciel <marciojosemacieljunior609@gmail.com>
2026-06-23 23:40:13 -0300

    [FEAT] Página 404

---
+++ src/view/404.php
@@ -0,0 +1,8 @@
+<div class="error-container">
+    <div class="error-content">
+        <h1 class="error-code">404</h1>
+        <h2 class="error-title">Página não encontrada</h2>
+        <p class="error-text">A página que você está procurando pode ter sido removida, mudou de nome ou está temporariamente indisponível.</p>
+        <a href="<?= BASE_URL; ?>" class="btn-return">Voltar para a Home</a>
+    </div>
+</div>
